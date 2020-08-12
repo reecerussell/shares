91c683be1c877c999231ac45db93688534b804be
Reece Russell <reece@prominentmedia.com>
2020-08-12 21:02:40 +0100

    moved auth dtos to core project

---
+++ Shares.Core/Dtos/AccessTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Shares.Core.Dtos
+{
+    public class AccessTokenDto
+    {
+        public string Token { get; set; }
+        public float Expires { get; set; }
+    }
+}
---
+++ Shares.Core/Dtos/UserCredentialDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Shares.Core.Dtos
+{
+    public class UserCredentialDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
