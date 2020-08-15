4592e0e01e9ce91ea5d5d44c86f49a19157d7f45
Reece Russell <reece@prominentmedia.com>
2020-08-15 14:19:49 +0100

    added entity framework read and write contexts

---
+++ Orders/Infrastructure/OrdersContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shares.Core;
+using Shares.Orders.Infrastructure.Configuration;
+
+namespace Shares.Orders.Infrastructure
+{
+    public class OrdersContext : DbContext
+    {
+        private readonly IConnectionStringProvider _provider;
+
+        public OrdersContext(IConnectionStringProvider provider)
+        {
+            _provider = provider;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new InstrumentConfiguration());
+            modelBuilder.ApplyConfiguration(new SellOrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_provider.Get());
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Orders/Infrastructure/OrdersReadContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Shares.Core;
+using Shares.Orders.Infrastructure.Configuration;
+
+namespace Shares.Orders.Infrastructure
+{
+    public class OrdersReadContext : DbContext
+    {
+        private readonly IConnectionStringProvider _provider;
+
+        public OrdersReadContext(IConnectionStringProvider provider)
+        {
+            _provider = provider;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new InstrumentConfiguration());
+            modelBuilder.ApplyConfiguration(new SellOrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.UseMySQL(_provider.Get());
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
