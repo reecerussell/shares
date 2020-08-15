730d409a815a49d30685970e0cd55d3b307b0a24
Reece Russell <reece@prominentmedia.com>
2020-08-15 15:32:59 +0100

    initial creation of the Orders RPC API, with support order creation

---
+++ Orders/API/Handlers/OrderHandler.cs
@@ -0,0 +1,53 @@
+﻿using CSharpFunctionalExtensions;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Shares.Core.Dtos;
+using Shares.Core.Services;
+using Shares.Orders.Infrastructure.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Shares.Orders.API.Handlers
+{
+    public class OrderHandler : OrderService.OrderServiceBase
+    {
+        private readonly IOrderService _service;
+        private readonly ILogger<OrderHandler> _logger;
+
+        public OrderHandler(
+            IOrderService service,
+            ILogger<OrderHandler> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        public override async Task<CreateOrderResponse> Create(CreateOrderRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Received incoming RPC to OrderService.Create");
+
+            try
+            {
+                var dto = new CreateOrderDto
+                {
+                    UserId = request.UserId,
+                    InstrumentId = request.InstrumentId,
+                    Price = request.Price,
+                    Quantity = request.Quantity
+                };
+                var (success, _, id, error) = await _service.CreateAsync(dto);
+                if (!success)
+                {
+                    return new CreateOrderResponse{Error = error};
+                }
+
+                return new CreateOrderResponse {Id = id};
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return new CreateOrderResponse{Error = e.Message};
+            }
+        }
+    }
+}
---
+++ Orders/API/Program.cs
@@ -0,0 +1,50 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Shares.AWS;
+using Shares.Core;
+using Shares.Core.Extensions;
+using Shares.Core.Services;
+using Shares.Orders.API.Handlers;
+using Shares.Orders.Infrastructure;
+using System;
+
+namespace Shares.Orders.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (!int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var port))
+            {
+                port = 8443;
+            }
+
+            var services = RegisterServices();
+            var orderHandler = services.GetRequiredService<OrderHandler>();
+
+            var server = new Server
+            {
+                Services = { OrderService.BindService(orderHandler) },
+                Ports = { new ServerPort("0.0.0.0", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Token server listening on port " + port);
+
+            // Keep the application running, to allow incoming RPC calls.
+            // Cannot use Console.ReadKey as Docker Compose doesn't support reading from console.
+            while (true) { }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        private static IServiceProvider RegisterServices() => new ServiceCollection()
+            .ConfigureConfig()
+            .ConfigureCache()
+            .ConfigureLogging()
+            .RegisterCoreServices()
+            .ConfigureAws()
+            .ConfigureInfrastructure()
+            .AddScoped<OrderHandler>()
+            .BuildServiceProvider();
+    }
+}
