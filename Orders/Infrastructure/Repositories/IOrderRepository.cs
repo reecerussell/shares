bb513d8aed4e1a67b0d9b678c82fcf5f21efad7f
Reece Russell <reece@prominentmedia.com>
2020-08-15 14:44:19 +0100

    created an order repository

---
+++ Orders/Infrastructure/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using Shares.Orders.Domain.Models;
+using System.Threading.Tasks;
+
+namespace Shares.Orders.Infrastructure.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Maybe<Order>> FindByIdAsync(string id);
+        void Add(Order order);
+        Task<Result> SaveChangesAsync();
+    }
+}
---
+++ Orders/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using Shares.Core.Entity;
+using Shares.Orders.Domain.Models;
+
+namespace Shares.Orders.Infrastructure.Repositories
+{
+    internal class OrderRepository : WriteRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(
+            OrdersContext context, 
+            ILogger<WriteRepository<Order>> logger) 
+            : base(context, logger)
+        {
+        }
+    }
+}
