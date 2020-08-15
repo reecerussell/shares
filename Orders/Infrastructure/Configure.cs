8b6a3f71ede3758f7274b7ec247d6d2daf821a3e
Reece Russell <reece@prominentmedia.com>
2020-08-15 15:13:16 +0100

    created an extension for IServiceCollection to register dependencies

---
+++ Orders/Infrastructure/Configure.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shares.Orders.Infrastructure.Repositories;
+using Shares.Orders.Infrastructure.Services;
+
+namespace Shares.Orders.Infrastructure
+{
+    public static class Configure
+    {
+        public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<OrdersContext>()
+                .AddScoped<OrdersReadContext>()
+                .AddTransient<IInstrumentReadRepository, InstrumentReadRepository>()
+                .AddTransient<IUserReadRepository, UserReadRepository>()
+                .AddTransient<IOrderRepository, OrderRepository>()
+                .AddTransient<IOrderService, OrderService>();
+        }
+    }
+}
