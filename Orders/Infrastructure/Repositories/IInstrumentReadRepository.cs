d793d02e3e22179fc33a172b5d62c4bd1b4c3b08
Reece Russell <reece@prominentmedia.com>
2020-08-15 14:31:10 +0100

    Added a read repo for the instrument domain

---
+++ Orders/Infrastructure/Repositories/IInstrumentReadRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Shares.Orders.Infrastructure.Repositories
+{
+    public interface IInstrumentReadRepository
+    {
+        Task<bool> ExistsAsync(string id);
+    }
+}
---
+++ Orders/Infrastructure/Repositories/InstrumentReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shares.Core.Entity;
+using Shares.Orders.Domain.Models;
+using System.Threading.Tasks;
+
+namespace Shares.Orders.Infrastructure.Repositories
+{
+    internal class InstrumentReadRepository : ReadRepository<Instrument>
+    {
+        public InstrumentReadRepository(OrdersReadContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<bool> ExistsAsync(string id)
+        {
+            return await Set.AnyAsync(x => x.Id == id);
+        }
+    }
+}
